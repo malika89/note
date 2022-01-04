b68041dca5a57a3f6732d4df600c4ae3947f31d6
momo_ai <momo_ai@noreply.gitea.io>
2022-01-04 18:13:47 +0800

    添加 'k8s/crd.go'

---
+++ k8s/crd.go
@@ -0,0 +1,6 @@
+package k8s
+
+//CRD 以及controller 编写
+// code :https://juejin.cn/post/6844904199029784589
+// https://cloud.tencent.com/developer/user/4071437
+// https://www.jianshu.com/u/a004b422adae
