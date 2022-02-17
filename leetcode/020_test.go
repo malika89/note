9ec7539f864e3811fe503ae363286cbfe10a3170
momo_ai <momo_ai@noreply.gitea.io>
2022-02-17 18:19:50 +0800

    添加 'leetcode/020_test.go'

---
+++ leetcode/020_test.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+//有效的括号
+
+//hash法 {[  ]}
+func isValid(s string) bool {
+	hash := map[byte]byte{')':'(', ']':'[', '}':'{'}
+	stack := make([]byte, 0)
+	if s == "" {
+		return true
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+		} else if len(stack) > 0 && stack[len(stack)-1] == hash[s[i]] {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
+
+func TestValid(t *testing.T)  {
+	str :="()[]{}"
+	t.Log(isValid(str))
+	str = "([)]"
+	t.Log(isValid(str))
+
+}
